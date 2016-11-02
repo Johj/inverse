536abe01b91f18b976129b691671fef328384ffa
Johj <peter.han.9493@gmail.com>
2016-11-02 02:13:37 -0700

    toggling kept adding style tags. now it removes.

--- inverse.js
+++ inverse.js
@@ -3,19 +3,19 @@ if(!window.counter) {
 	window.counter = 0;
 }
 
-var css = window.counter % 2 ?
-	'html {-webkit-filter: invert(0%);' +
-	'-moz-filter: invert(0%);' +
-	'-o-filter: invert(0%);' +
-	'-ms-filter: invert(0%);}' :
-	'html {-webkit-filter: invert(100%);' +
-	'-moz-filter: invert(100%);' +
-	'-o-filter: invert(100%);' +
-	'-ms-filter: invert(100%);}';
+if(window.counter % 2) {
+	var element = document.getElementById("invert");
+	element.parentNode.removeChild(element);
+} else {
+	var css = 'html {-webkit-filter: invert(100%);' +
+		'-moz-filter: invert(100%);' +
+		'-o-filter: invert(100%);' +
+		'-ms-filter: invert(100%); }',
+	head = document.getElementsByTagName('head')[0],
+	style = document.createElement('style');
+
+	style.id = 'invert';
+	style.appendChild(document.createTextNode(css));
+	head.appendChild(style);
+}
 ++window.counter;
-
-var head = document.getElementsByTagName('head')[0],
-		style = document.createElement('style');
-
-style.appendChild(document.createTextNode(css));
-head.appendChild(style);
